eda71db8a41e2cf42a317eaaad8812248dbb4f50
xStranged <fede.valle04@gmail.com>
2024-09-27 20:53:57 -0300

    feat: :sparkles: service createUser
    to register a new user

---
+++ services/user.ts
@@ -0,0 +1,66 @@
+import { API_URL } from "@/constants/const";
+import { UserAccount } from "@/types/input";
+
+
+export const createUser = async (userAccount: UserAccount) => {
+
+    console.log("API_URL");
+    console.log(API_URL);
+
+    const url = 'http://localhost:5000/users';
+    const data = {
+        nombre: "Fede",
+        apellido: "valle",
+        email: "santiagofirz97@gmail.com",
+        password: "test1234",
+        username: "santiagof",
+        validacionMail: 0
+    };
+
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // Convertir meetingDTO a una cadena JSON
+    };
+
+    try {
+        const res = await fetch('http://localhost:5000/users', options);
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        // const response = JSON.stringify(res)
+        // console.log(response);
+
+        if (res.status == 201) {
+            return res.json()
+        } else return false
+
+    } catch (error) {
+        console.log(error);
+
+        return false;
+    }
+
+
+
+
+    // fetch(url, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data)
+    // })
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             throw new Error('Network response was not ok ' + response.statusText);
+    //         }
+    //         return response.json();
+    //     })
+    //     .catch((error) => {
+    //         console.error('Error:', error);
+    //     });
+}
