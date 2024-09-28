2f80205f04e43a95aebc576eb6920eb8135954b6
zoequiroz3 <zoequiroz03@gmail.com>
2024-09-28 20:24:53 -0300

    Agrego 2 campos user y password, y arreglé el tamaño del boton registro

--- app/(account)/login.tsx
+++ app/(account)/login.tsx
@@ -1,12 +1,60 @@
 import { Link } from "expo-router"
 import { Text, View } from "react-native"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar"
+import { Button } from "@/components/Button";
+import { Input } from "@/components/inputs/Input";
+import { InputStyled } from "@/components/inputs/InputStyled";
+import { useState } from "react";
+
+
 
 export default function LoginScreen() {
 
+    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return (
-        <View className="bg-slate-400 flex items-center justify-center h-screen pl-5 pr-5">
-            <Text className="text-gray-200">Login</Text>
-            <Link href='/(account)/register'>No tienes cuenta? registrarse</Link>
-        </View>
+        <View className="bg-gray-200 flex h-screen pl-7 pr-7 dark:bg-gray-900 ">
+        
+            <View className="self-center mt-8">
+                    <Avatar className="w-36 h-36">
+                        <AvatarImage
+                            className="w-36 h-36"
+                            source={require('../../assets/images/userlogoblack.png')}
+                        />
+                        <AvatarFallback>CG</AvatarFallback>
+                    </Avatar>
+            </View>
+
+            <View className="mt-5">
+                <Text className="text-md font-medium mb-2 dark:text-slate-100"
+                >Username</Text>
+                <InputStyled
+                    setValueInput={setUsername}
+                    placeholder="Ingrese su nombre de usuario"
+                />
+            </View>
+
+            <View className="mt-5 mb-5">
+                <Text className="text-md font-medium mb-2 dark:text-slate-100"
+                >Contraseña</Text>
+                <Input
+                    className="focus:border focus:border-slate-900 dark:focus:border-gray-400"
+                    secureTextEntry={true}
+                    value={password}
+                    onChange={(e) => setPassword(e.nativeEvent.text)}
+                    placeholder="Ingrese su contraseña"
+                />
+            </View>
+
+            <View className="bg-slate-400 flex items-center justify-center h-20 pl-5 pr-5">
+                <Text className="text-gray-200">Login</Text>
+                <Link href='/(account)/register'>No tienes cuenta? registrarse</Link>
+            </View>
+
+
+        </View>    
     )
 }
