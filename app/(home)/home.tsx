7ec7f702cef98915e5c61ec69b417f172aca57f2
xStranged <fede.valle04@gmail.com>
2024-10-16 16:05:52 -0300

    feat: :construction: login with auth0 universal login
    login with google, show credentials, access token type beared. dont work with auth0 in backend

--- app/(home)/home.tsx
+++ app/(home)/home.tsx
@@ -1,22 +1,59 @@
 import { router } from "expo-router";
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import { Button } from "@/components/buttons/Button";
-import { useAuth0 } from "react-native-auth0";
+import { Credentials, useAuth0 } from "react-native-auth0";
+import { useState } from "react";
+import { Input } from "@/components/inputs/Input";
 
 
 export default function HomeScreen() {
-    const { user, error } = useAuth0();
+    const { user, error, clearSession, getCredentials } = useAuth0();
+    const [credentials, setCredentials] = useState<Credentials | undefined>()
+    const [data, setData] = useState()
 
     const handleLogout = () => {
         router.replace("/(account)/login");
     }
-    const { clearSession } = useAuth0();
 
     const Logout = async () => {
         try {
             await clearSession();
         } catch (e) {
             console.log(e);
+        }
+    };
+
+    const getCred = async () => {
+        try {
+            const cred = await getCredentials();
+            setCredentials(cred)
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const getBearedToken = async () => {
+        try {
+
+        } catch (e) {
+            Alert.alert(e)
+        }
+    }
+
+    const getProtectedData = async () => {
+        try {
+            const response = await fetch('http://192.168.0.176:5000/protected', {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${credentials?.accessToken}`, // Usa el accessToken aquí
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const data = await response.json();
+            setData(data)
+        } catch (error) {
+            console.error('Error al acceder a la ruta protegida:', error);
         }
     };
 
@@ -27,9 +64,33 @@ export default function HomeScreen() {
             <View className="items-center mt-7 mb-5">
                 <Button className="w-52 bg-[#104736] text-stone-50" label="Cerrar Sesión" onPress={handleLogout} />
                 {user && <Text>Logged in as {user.name}</Text>}
+                {user && <Text>Sub {user.sub}</Text>}
+
+
+
+                {credentials && <Text>Credential expiresAt{credentials.expiresAt}</Text>}
+                {credentials?.accessToken && (
+                    <Input className="focus:border focus:border-slate-900 dark:focus:border-gray-400"
+                        value={credentials.expiresAt.toString()}
+                    />
+                )}
+                {credentials?.idToken && (
+                    <Input className="focus:border focus:border-slate-900 dark:focus:border-gray-400"
+                        value={credentials.idToken}
+                    />
+                )}
+                {credentials?.refreshToken && <Text>Credential refreshToken{credentials.refreshToken}</Text>}
+                {credentials?.scope && <Text>Credential scope{credentials.scope}</Text>}
+                {credentials?.tokenType && <Text>Credential tokenType{credentials.tokenType}</Text>}
+
                 {!user && <Text>Not logged in</Text>}
                 {error && <Text>{error.message}</Text>}
                 <Button label="Logout" onPress={Logout} />
+                <Button label="GetCredentials" onPress={getCred} />
+                <Button label="getBearedToken" onPress={getBearedToken} />
+                <Button label="getProtectedData" onPress={getProtectedData} />
+                {data && <Text>data: {data}</Text>}
+
             </View>
         </View>
     );
