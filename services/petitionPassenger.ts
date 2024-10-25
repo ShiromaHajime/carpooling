55fe7235dd07c87c7e8608c2edb76c7365fcbad8
zoequiroz3 <126729283+zoequiroz3@users.noreply.github.com>
2024-10-25 01:07:20 -0300

    arreglo codigo fusion en 1 aceptar y cancelar (decision ft)

--- services/petitionPassenger.ts
+++ services/petitionPassenger.ts
@@ -3,8 +3,39 @@ import { Response, Response500 } from "@/types/errors";
 import { Trip, TripById } from "@/types/types";
 
 
-export const acceptPassenger= (id_trip: number, id_user: string) => {
+export const acceptPassenger= async (id_trip: number, id_user: string, decision: boolean) => {
+    
+    console.log("API_URL de la variable de entorno");
+    console.log(API_URL);
 
+    const body = {
+        id_trip: id_trip,
+        passenger_id: id_user,
+        decision: decision,
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    };
+
+    try {
+        const res = await fetch(`${API_URL}/trip_join`, options);
+        if (res.status == 200) {
+            const data = await res.json()
+            const response: Response = {
+                data: data
+            }
+            return response
+        } else return Response500
+
+    } catch (error) {
+
+        return Response500;
+    }
     return true
 }
 
