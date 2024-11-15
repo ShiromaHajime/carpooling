36c6a6cf480e8b27f9a963361b87097d8d52f48b
xStranged <fede.valle04@gmail.com>
2024-11-15 16:44:32 -0300

    style: :lipstick:

--- app/home/trips/detail/CardPassenger.tsx
+++ app/home/trips/detail/CardPassenger.tsx
@@ -5,43 +5,56 @@ import { Text, View } from "react-native"
 import { AvatarImage } from "@/components/Avatar";
 import { Toggle } from "@/components/Toggle"
 import { Toast } from "@/components/Toast"
+import { router } from "expo-router";
+import { useColorScheme } from "nativewind";
 
-
+interface Passanger {
+    rating: number,
+    rating_count: number,
+    user: User
+}
 interface PropCard {
-    passenger: User,
+    passenger: Passanger,
     isSimple: boolean,
     title: string,
     handleDecision: (desicion: boolean, id_user: string) => void,
 }
 
 export const CardPassenger = ({ passenger, isSimple, title, handleDecision }: PropCard) => {
-    console.log("passenger", passenger);
+    const { colorScheme } = useColorScheme()
+    const avatarBlack = require('@/assets/images/user-avatar-black.png')
+    const avatarWhite = require('@/assets/images/user-avatar-white.png')
     return (
         <View className="flex gap-2">
             <Text className="font-semibold text-xl text-foreground">{title}</Text>
             <Card>
                 <CardHeader>
                     <View className="flex flex-row justify-between items-center">
-                        <View>
+                        <View className="mr-2">
                             <AvatarImage
                                 className="w-16 h-16"
-                                source={require('@/assets/images/userlogoblack.png')}
+                                source={colorScheme == 'dark' ? avatarWhite : avatarBlack}
                             />
                         </View>
                         <View className="flex flex-row items-center">
-                            <CardTitle>{passenger.first_name} {passenger.last_name}</CardTitle>
+                            <CardTitle>{passenger.user.first_name} {passenger.user.last_name}</CardTitle>
                         </View>
-                        {(!isSimple) && (
-                            <View>
-                                <Button className="bg-background border border-secondary" labelClasses="text-foreground" label="ACEPTAR" onPress={() => handleDecision(true, passenger.id)} />
-                                <Button className="bg-background border border-secondary" labelClasses="text-foreground" label="RECHAZAR" onPress={() => handleDecision(false, passenger.id)} />
-                            </View>
-                        )}
+
                     </View>
+
+                    {(!isSimple) && (
+                        <View>
+                            <Button className="bg-background border border-secondary" labelClasses="text-foreground" label="ACEPTAR" onPress={() => handleDecision(true, passenger.user.id)} />
+                            <Button className="bg-background border border-secondary" labelClasses="text-foreground" label="RECHAZAR" onPress={() => handleDecision(false, passenger.user.id)} />
+                        </View>
+                    )}
                     <CardDescription></CardDescription>
+
                 </CardHeader>
                 <CardFooter className="flex flex-row justify-between items-center flex-wrap">
-                    <Button className="bg-background border border-secondary" labelClasses="text-foreground" label="Ver perfil" />
+                    <Button className="bg-background border border-secondary" labelClasses="text-foreground" label="Ver perfil"
+                        onPress={() => router.navigate({ pathname: "/home/profile", params: { idDriver: passenger.user.id } })}
+                    />
                 </CardFooter>
             </Card>
         </View>
